daee489749e217591c50b8bf6b8e4fbfdcf04682
Aziz Akbarov <azizbakbarov@gmail.com>
2022-01-10 08:58:49 +0500

    Adding useFetch

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,32 +1,36 @@
-import { useState, useEffect } from "react";
-
+import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
 //styles
 import "./TripList.css";
 
 export default function TripList() {
-  const [trips, setTrips] = useState([]);
-
-  useEffect(() => {
-    fetch(" http://localhost:3000/trips")
-      .then((response) => response.json())
-      .then((json) => setTrips(json));
-  }, []);
-
-  console.log(trips);
+  const [url, setUrl] = useState(" http://localhost:3000/trips");
+  const { data: trips } = useFetch(url);
 
   return (
     <div className="trip-list">
       <h2>Trip List </h2>
       <ul>
-        {trips.map((trip) => {
-          return (
-            <li key={trip.id}>
-              <h3>{trip.title}</h3>
-              <p>{trip.price}</p>
-            </li>
-          );
-        })}
+        {trips &&
+          trips.map((trip) => {
+            return (
+              <li key={trip.id}>
+                <h3>{trip.title}</h3>
+                <p>{trip.price}</p>
+              </li>
+            );
+          })}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl(" http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl(" http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 }
